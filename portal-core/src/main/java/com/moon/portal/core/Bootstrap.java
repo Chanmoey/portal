7c4f2b11833472ac9f06c8c86cc35637b20f3c6e
Chanmoey <relijifa@gmail.com>
2023-06-02 03:11:13 +0800

    Chanmoey feat: Define the gateway bootstrap.

---
+++ portal-core/src/main/java/com/moon/portal/core/Bootstrap.java
@@ -0,0 +1,22 @@
+package com.moon.portal.core;
+
+/**
+ * @author Chanmoey
+ * @date 2023年06月01日
+ */
+public class Bootstrap {
+
+    public static void main(String[] args) {
+        // 加载静态配置
+
+        // 初始化网关插件
+
+        // 加载配置中心管理器，获取动态配置，监听配置中心
+
+        // 启动容器
+
+        // 连接注册中心，将注册中心的实例加载到本地
+
+        // 服务关机
+    }
+}
