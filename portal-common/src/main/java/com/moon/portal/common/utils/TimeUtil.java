bb0ccd9da7e7330ed0bef3083af03b1b58da7b60
Chanmoey <relijifa@gmail.com>
2023-06-02 04:05:02 +0800

    Chanmoey feat: 实现时间相关工具类.

---
+++ portal-common/src/main/java/com/moon/portal/common/utils/TimeUtil.java
@@ -0,0 +1,12 @@
+package com.moon.portal.common.utils;
+
+/**
+ * @author Chanmoey
+ * @date 2023年06月02日
+ */
+public class TimeUtil {
+
+    public long currentTimeMillis() {
+        return System.currentTimeMillis();
+    }
+}
